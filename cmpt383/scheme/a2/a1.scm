2f0245be906d9c8025003986f06b5595e7971298
bmh6 <bmh6@cs-sc4080u-c01.cs.surrey.sfu.ca>
2018-01-29 12:49:47 -0800

    q2 done

--- cmpt383/scheme/a2/a1.scm
+++ cmpt383/scheme/a2/a1.scm
@@ -1,5 +1,17 @@
-(define (singleton? x)
-	(cond ((null? (cdr x)) #t)
- 		(else #f)
+(define singleton?
+	(lambda (x)
+		(cond ((null? (cdr x)) #t)
+				(else #f)
+		)
 	)
+)
+
+(define make-my-list
+	(lambda (n x)
+		(cond
+			(( = n 0) '())
+			(else 
+				(cons x (make-my-list (- n 1) x)))
+		)
+	)	
 )
