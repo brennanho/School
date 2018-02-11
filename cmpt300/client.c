75026f9ed1ab28cbc8ee88851482661a5fe22386
unknown <brennanmho@gmail.com>
2018-02-11 01:12:19 -0800

    f

--- cmpt300/client.c
+++ cmpt300/client.c
@@ -2,7 +2,35 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netdb.h>
 #include <string.h>
+#include <pthread.h>
+
+//http://www.binarytides.com/hostname-to-ip-address-c-sockets-linux/
+int hostname_to_ip(char * hostname , char* ip)
+{
+    struct hostent *he;
+    struct in_addr **addr_list;
+    int i;
+         
+    if ( (he = gethostbyname( hostname ) ) == NULL) 
+    {
+        // get the host info
+        herror("gethostbyname");
+        return 1;
+    }
+ 
+    addr_list = (struct in_addr **) he->h_addr_list;
+     
+    for(i = 0; addr_list[i] != NULL; i++) 
+    {
+        //Return the first one;
+        strcpy(ip , inet_ntoa(*addr_list[i]) );
+        return 0;
+    }
+     
+    return 1;
+}
 
 //Client socket testing
 int main(int argc, char *argv[]) {
@@ -10,24 +38,27 @@ int main(int argc, char *argv[]) {
 	int bufLen = 1024; 
 	char buf[bufLen];
 	char msg[bufLen];
-	struct sockaddr_in siRemote; // Server info?
-	int sLen = sizeof(siRemote);
+    struct sockaddr_in server; // Server info?
+	int sLen = sizeof(server);
 
-	siRemote.sin_family = AF_INET;
-    siRemote.sin_port = htons(8888); // test port
+	server.sin_family = AF_INET;
+    server.sin_port = htons(8888); // test port
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	int sock = socket(AF_INET, SOCK_DGRAM, 0); // why 0?
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	inet_aton("127.0.0.1", &siRemote.sin_addr);
+	while (connect(sock, (struct sockaddr *) &server, sizeof(server)) == -1); //attemptin to connect to remote machine
+
+	printf("Succesfully connected to remote socket\n");
 
 	while (1) {
 
 		printf("Client Input: ");
 		fgets(msg, sizeof(msg), stdin);
 
-		sendto(sock, msg, strlen(msg), 0, (struct sockaddr*) &siRemote, sLen);
+		sendto(sock, msg, strlen(msg), 0, (struct sockaddr*) &server, sLen);
 
-		recvfrom(sock, buf, bufLen, 0, (struct sockaddr*) &siRemote, &sLen);
+		recvfrom(sock, buf, bufLen, 0, (struct sockaddr*) &server, &sLen);
 
 		printf("Server Reply: %s", buf);
 
--- cmpt300/server.c
+++ cmpt300/server.c
@@ -2,31 +2,33 @@
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-//Server socket testing
+//server socket testing
 int main(void) {
 
 	int bufLen = 1024; 
 	char buf[bufLen];
-	struct sockaddr_in siLocal, siRemote;
-	int sLen = sizeof(siRemote);
+	struct sockaddr_in server, meClient, friendClient;
+	int sLen = sizeof(meClient);
 
-	siLocal.sin_family = AF_INET;
-    siLocal.sin_port = htons(8888); // test port
-    siLocal.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_family = AF_INET;
+    server.sin_port = htons(8888); // test port
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-	int sock = socket(AF_INET, SOCK_DGRAM, 0); // why 0?
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	bind(sock, (struct sockaddr*) &siLocal, sizeof(siLocal)); //what exactly is siLocal?
+	while (!bind(sock, (struct sockaddr*) &server, sizeof(server))); //Attempting to bind socket...
+
+	printf("Successfully binded socket\n");
 
 	while (1) {
 
-		int recvLen = recvfrom(sock, buf, bufLen, 0, (struct sockaddr*) &siRemote, &sLen);
+		int recvLen = recvfrom(sock, buf, bufLen, 0, (struct sockaddr*) &friendClient, &sLen);
 
 		printf("Client: %s", buf);
 
-		sendto(sock, buf, recvLen, 0, (struct sockaddr*) &siRemote, sLen);
-
+		sendto(sock, buf, recvLen, 0, (struct sockaddr*) &meClient, sLen);
 
 	}
 
