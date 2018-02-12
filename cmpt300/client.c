a05d3f7c5d7b09da84e24e82bd6a0b2a3005f9e7
unknown <brennanmho@gmail.com>
2018-02-12 13:11:26 -0800

    p2p now working

--- cmpt300/client.c
+++ cmpt300/client.c
@@ -6,69 +6,61 @@
 #include <string.h>
 #include <pthread.h>
 
-typedef struct sendRecvInfo {
+typedef struct p2pClient {
     int sock;
     char* msgSend;
     char* msgRecv;
     int msgLen;
-    struct sockaddr_in server;
+    struct sockaddr_in friendClient;
     int sLen;
-} sendRecvInfo;
+} p2pClient;
 
-void* sendMessage(void* serverInfoptr) {
-    sendRecvInfo serverInfo = *(sendRecvInfo*)serverInfoptr;
-    while (!bind(serverInfo.sock, (struct sockaddr*) &(serverInfo.server), sizeof(serverInfo.server)));
-    while (connect(serverInfo.sock, (struct sockaddr *) &(serverInfo.server), sizeof(serverInfo.server)) == -1); //attemptin to connect to remote machine
+void* sendMessage(void* p2pInfoPtr) {
+    p2pClient p2pInfo = *(p2pClient*)p2pInfoPtr;
     while (1) {
-        printf("myClient Input: ");
-        fgets(serverInfo.msgSend, sizeof(serverInfo.msgSend), stdin);
-        sendto(serverInfo.sock, serverInfo.msgSend, strlen(serverInfo.msgSend), 0, (struct sockaddr*) &(serverInfo.server), serverInfo.sLen);
+        fgets(p2pInfo.msgSend, sizeof(p2pInfo.msgSend), stdin);
+        sendto(p2pInfo.sock, p2pInfo.msgSend, strlen(p2pInfo.msgSend), 0, (struct sockaddr*) &(p2pInfo.friendClient), p2pInfo.sLen);
     }
     return NULL;
 }
 
-void* recvMessage(void* serverInfoptr) {
-    sendRecvInfo serverInfo = *(sendRecvInfo*)serverInfoptr;
-    while (!bind(serverInfo.sock, (struct sockaddr*) &(serverInfo.server), sizeof(serverInfo.server)));
-    while (connect(serverInfo.sock, (struct sockaddr *) &(serverInfo.server), sizeof(serverInfo.server)) == -1); //attemptin to connect to remote machine
+void* recvMessage(void* p2pInfoPtr) {
+    p2pClient p2pInfo = *(p2pClient*)p2pInfoPtr;
     while (1) {
-        memset(serverInfo.msgRecv,'\0', serverInfo.msgLen); // clear previous message
-        recvfrom(serverInfo.sock, serverInfo.msgRecv, serverInfo.msgLen, 0, (struct sockaddr*) &(serverInfo.server), &(serverInfo.sLen));
-        printf("Friend's reply: %s", serverInfo.msgRecv);
+        memset(p2pInfo.msgRecv,'\0', p2pInfo.msgLen); // clear previous message
+        recvfrom(p2pInfo.sock, p2pInfo.msgRecv, p2pInfo.msgLen, 0, (struct sockaddr*) &(p2pInfo.friendClient), &(p2pInfo.sLen));
+        printf("Friend's reply: %s", p2pInfo.msgRecv);
     }
     return NULL;
 }
 
-//myClient socket testing
+//p2p UDP socket testing
 int main(int argc, char *argv[]) {
 
     pthread_t sendThread, recvThread;
     int msgRecvLen = 1024; 
     char msgRecv[msgRecvLen];
     char msg[msgRecvLen];
-    struct sockaddr_in myServer, friendServer, myClient;
-    int sLen = sizeof(myServer);
-
-    myServer.sin_family = AF_INET;
-    myServer.sin_port = htons(8888); // test port
-    myServer.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    friendServer.sin_family = AF_INET;
-    friendServer.sin_port = htons(8886); // test port
-    friendServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+    struct sockaddr_in friendClient, myClient;
+    int sLen = sizeof(myClient);
 
     myClient.sin_family = AF_INET;
-    myClient.sin_port = htons(8889);
+    myClient.sin_port = htons(8887);
     myClient.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+    friendClient.sin_family = AF_INET;
+    friendClient.sin_port = htons(8889);
+    friendClient.sin_addr.s_addr = inet_addr("127.0.0.1");
+
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    int sock2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    sendRecvInfo serverInfoSend = {sock, msg, msgRecv, msgRecvLen, myServer, sLen};
-    sendRecvInfo serverInfoRecv = {sock2, msg, msgRecv, msgRecvLen, friendServer, sLen};
+    p2pClient p2pInfo = {sock, msg, msgRecv, msgRecvLen, friendClient, sLen};
 
-    pthread_create(&sendThread, NULL, sendMessage, &serverInfoSend);
-    pthread_create(&recvThread, NULL, recvMessage, &serverInfoRecv);
+    while (!bind(sock, (struct sockaddr *) &(myClient), sizeof(myClient)));
+    while (connect(sock, (struct sockaddr *) &(friendClient), sizeof(friendClient)) == -1);
+
+    pthread_create(&sendThread, NULL, sendMessage, &p2pInfo);
+    pthread_create(&recvThread, NULL, recvMessage, &p2pInfo);
 
     pthread_join(sendThread, NULL);
     pthread_join(recvThread, NULL);
