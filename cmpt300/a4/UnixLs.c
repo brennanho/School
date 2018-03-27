de49dfb928e412ca94c598c621208b79cc1761c7
brennanho <brennanmho@gmail.com>
2018-03-27 01:11:56 -0700

    yooo

--- cmpt300/a4/UnixLs.c
+++ cmpt300/a4/UnixLs.c
@@ -8,6 +8,13 @@
 #include <stdlib.h>
 #include "list.h"
 
+//Helper for printing files when they are found as command line arguments
+int is_file(char* path) {
+    struct stat buf;
+    stat(path, &buf);
+    return S_ISREG(buf.st_mode);
+}
+
 //Unix print what ls returns with different parameters
 int printls(struct dirent* de, char* param, char* fullPath) {
 	if (strchr(param, 'l') != NULL) {
@@ -32,7 +39,7 @@ int printls(struct dirent* de, char* param, char* fullPath) {
         printf( (itemStats->st_mode & S_IWOTH) ? "w" : "-");
         printf( (itemStats->st_mode & S_IXOTH) ? "x" : "-");
 
-        printf("%d %s %s %d %s %s\n", itemStats->st_nlink, owner->pw_name, group->gr_name, itemStats->st_size, date, de->d_name);
+        printf("%d %s %s %011d %s %s\n", itemStats->st_nlink, owner->pw_name, group->gr_name, itemStats->st_size, date, de->d_name);
         free(itemStats);
     }
     else if (strchr(param, 'i') != NULL)
@@ -47,16 +54,21 @@ int ls(char* dir, char* param) {
 	 
 	struct dirent* de;
     DIR *dr = opendir(dir);
- 
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory
+    
+    if (is_file(dir) != 0) {
+        printf("\n%s\n",dir);
+        return 0;
+    } else if (dr == NULL) { // opendir returns NULL if couldn't open directory 
+        printf("UnixLs: cannot access '%s': No such file or directory\n", dir);
         return -1;
+    } 
  	
  	LIST* subDirs = ListCreate();
 
  	//Print contents of current working directory and adds any sub directory to a list of sub directories
     while ((de = readdir(dr)) != NULL) {
     	if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
-        	char* subDirName = malloc(1024*sizeof(char));
+        	char* subDirName = malloc(255*sizeof(char));
         	strcpy(subDirName, dir);
         	strcat(subDirName, "/");
         	strcat(subDirName, de->d_name);
@@ -76,6 +88,7 @@ int ls(char* dir, char* param) {
     	free(subDir);
     }
 
+
     ListFree(subDirs, itemFree);
    	closedir(dr);
 
@@ -84,19 +97,46 @@ int ls(char* dir, char* param) {
  
 int main(int argc, char* argv[])
 {
-    char* param;
+
     char arguments[128];
     memset(arguments, '\0', 128);
-    
+
+    LIST* directoryArgs = ListCreate();
+
+    //Iterates through command line arguments and parses appropriate parameter options. Also appends files/folders to directoryArgs list to be processed
     for (int i = 1; argv[i] != NULL; i++) {
-        strcat(arguments, argv[i]);
-        if (argv[i+1] != NULL)
-            strcat(arguments, " ");
+        if (argv[i][0] == '-') {
+            strcat(arguments, argv[i]);
+            if (argv[i+1] != NULL)
+                strcat(arguments, " ");
+        } else {
+            char* fileFolder = calloc(255, 255*sizeof(char));
+            strcpy(fileFolder, argv[i]);
+            ListPrepend(directoryArgs, fileFolder);
+        }  
     }
 
-    printf("%s\n",arguments);
-
-    ls(".", arguments);
+    //Call ls on all directories/files in directoryArgs with appropriate parameters
+    if (ListCount(directoryArgs) == 0)
+        ls(".", arguments);
+    else if (ListCount(directoryArgs) == 1) {
+        char* fileFolder = ListTrim(directoryArgs);
+        ls(fileFolder, arguments);
+        free(fileFolder);
+    } else {
+        char* fileFolder = ListTrim(directoryArgs);
+        if (is_file(fileFolder) == 0)
+            printf("%s:\n", fileFolder);
+        ls(fileFolder, arguments);
+        free(fileFolder);
+        while (ListCount(directoryArgs) > 0) {
+            fileFolder = ListTrim(directoryArgs);
+            if (is_file(fileFolder) == 0)
+                printf("\n%s:\n", fileFolder);
+            ls(fileFolder, arguments);
+            free(fileFolder);
+        }
+    }
  
     return 0;
 }
