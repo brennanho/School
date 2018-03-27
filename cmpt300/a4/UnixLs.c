ac49e4db87bcb0ff536fe4678f1a7b0b1d876c2a
brennanho <brennanmho@gmail.com>
2018-03-26 23:21:34 -0700

    a4 almost done

--- cmpt300/a4/UnixLs.c
+++ cmpt300/a4/UnixLs.c
@@ -9,39 +9,40 @@
 #include "list.h"
 
 //Unix print what ls returns with different parameters
-int printls(struct dirent* de, char* param) {
-	if (strcmp(param, "i") == 0)
-		printf("%lld %s\n",de->d_ino, de->d_name);
-	else if (strcmp(param, "l") == 0) {
-        struct stat itemStats; //file or folder
-        stat(de->d_name, &itemStats);
-        struct group *group = getgrgid(itemStats.st_gid);
-        struct passwd *owner = getpwuid(itemStats.st_uid);
+int printls(struct dirent* de, char* param, char* fullPath) {
+	if (strchr(param, 'l') != NULL) {
+        struct stat* itemStats = calloc(4, sizeof *itemStats);
+        stat(fullPath, itemStats);
+        struct passwd* owner = getpwuid(itemStats->st_uid);
+        struct group* group = getgrgid(itemStats->st_gid);
         char date[20];
-        strftime(date, 20, "%b %d %H:%M", localtime(&(itemStats.st_ctime)));
+        strftime(date, 20, "%b %d %Y %H:%M", localtime(&(itemStats->st_ctime)));
 
-        printf( (S_ISDIR(itemStats.st_mode)) ? "d" : "-");
-        printf( (itemStats.st_mode & S_IRUSR) ? "r" : "-");
-        printf( (itemStats.st_mode & S_IWUSR) ? "w" : "-");
-        printf( (itemStats.st_mode & S_IXUSR) ? "x" : "-");
-        printf( (itemStats.st_mode & S_IRGRP) ? "r" : "-");
-        printf( (itemStats.st_mode & S_IWGRP) ? "w" : "-");
-        printf( (itemStats.st_mode & S_IXGRP) ? "x" : "-");
-        printf( (itemStats.st_mode & S_IROTH) ? "r" : "-");
-        printf( (itemStats.st_mode & S_IWOTH) ? "w" : "-");
-        printf( (itemStats.st_mode & S_IXOTH) ? "x" : "-");
-		printf(" %d %s %s %d %s %s\n",itemStats.st_nlink, owner->pw_name, group->gr_name, itemStats.st_size, date, de->d_name); // (filetype flag, permissions, link count, owner account, group name, file size, date/time stamp, file name)
+        if (strchr(param, 'i') != NULL)
+            printf("%lld ",de->d_ino);
+
+        printf( (S_ISDIR(itemStats->st_mode)) ? "d" : "-");
+        printf( (itemStats->st_mode & S_IRUSR) ? "r" : "-");
+        printf( (itemStats->st_mode & S_IWUSR) ? "w" : "-");
+        printf( (itemStats->st_mode & S_IXUSR) ? "x" : "-");
+        printf( (itemStats->st_mode & S_IRGRP) ? "r" : "-");
+        printf( (itemStats->st_mode & S_IWGRP) ? "w" : "-");
+        printf( (itemStats->st_mode & S_IXGRP) ? "x" : "-");
+        printf( (itemStats->st_mode & S_IROTH) ? "r" : "-");
+        printf( (itemStats->st_mode & S_IWOTH) ? "w" : "-");
+        printf( (itemStats->st_mode & S_IXOTH) ? "x" : "-");
+
+        printf("%d %s %s %d %s %s\n", itemStats->st_nlink, owner->pw_name, group->gr_name, itemStats->st_size, date, de->d_name);
+        free(itemStats);
     }
-	else if (strcmp(param, "R") == 0)
-		printf("%s\n", de->d_name);
-	else if (strcmp(param, "\0") == 0)
-		 printf("%s\n", de->d_name);
+    else if (strchr(param, 'i') != NULL)
+        printf("%lld %s\n",de->d_ino, de->d_name);
 	else
-		return -1; //error bad parameter
+		printf("%s\n", de->d_name);
 	return 0;
 }
 
-//Unix ls instruction with no parameters 
+//Unix ls instruction (recursive)
 int ls(char* dir, char* param) {
 	 
 	struct dirent* de;
@@ -55,12 +56,12 @@ int ls(char* dir, char* param) {
  	//Print contents of current working directory and adds any sub directory to a list of sub directories
     while ((de = readdir(dr)) != NULL) {
     	if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
-        	printls(de, param);
         	char* subDirName = malloc(1024*sizeof(char));
         	strcpy(subDirName, dir);
         	strcat(subDirName, "/");
         	strcat(subDirName, de->d_name);
-        	if (opendir(subDirName) != NULL && strcmp(param,"R") == 0)
+            printls(de, param, subDirName);
+        	if (opendir(subDirName) != NULL && strchr(param,'R') != NULL)
         		ListPrepend(subDirs, subDirName);
         	else
         		free(subDirName);
@@ -84,14 +85,18 @@ int ls(char* dir, char* param) {
 int main(int argc, char* argv[])
 {
     char* param;
-
-    if (argv[1] == NULL)
-    	param = "\0";
-    else 
-    	param = argv[1];
-
-    ls(".", param);
- 
+    char arguments[128];
+    memset(arguments, '\0', 128);
     
+    for (int i = 1; argv[i] != NULL; i++) {
+        strcat(arguments, argv[i]);
+        if (argv[i+1] != NULL)
+            strcat(arguments, " ");
+    }
+
+    printf("%s\n",arguments);
+
+    ls(".", arguments);
+ 
     return 0;
 }
